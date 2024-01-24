cc66be3326c86d4763959c2f2fd30c72c567bf1c
divya <divyasivasankaran1998@gmail.com>
2024-01-24 17:52:29 +0530

    test

--- set01/problem03.c
+++ set01/problem03.c
@@ -1,1 +1,34 @@
-<>
+#include<stdio.h>
+
+int input();
+int add(int a, int b);
+void output(int a,int b, int sum);  //actual argument
+//void is only for printing it will not return any value.
+int main()
+{
+  int num1,num2,result;
+  num1 = input();
+  num2 = input();
+  result = add(num1,num2);  //formal argumrnts
+  output(num1,num2,result);
+  return 0;
+}
+
+int input()
+{
+  int num;
+  printf("Enter a number:");
+  scanf("%d",&num);
+  return num;
+}
+
+int add(int a, int b)
+{
+  return a + b;
+}
+
+void output(int a, int b, int sum)
+{
+  printf("The result is %d ",sum);
+  
+}
