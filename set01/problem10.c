0721ddd0004a55984fd2b270509903c6a9b9f3ea
divya <divyasivasankaran1998@gmail.com>
2024-02-06 12:47:33 +0530

    test

--- set01/problem10.c
+++ set01/problem10.c
@@ -23,16 +23,46 @@
 
   int stringcompare(char *string1, char *string2) 
   {
-    /*i want to find whether two given strings are equal,greater than or not equal.
+    int i,result =0;
+    for(i=0;string1[i]!='\0';i++)
+      {
+        while(string1[i]>string2[i])
+          {
+            result = 1;
+            break;
+          }
+        while(string1[i]<string2[i])
+          {
+            result = -1;
+            break;
+          }
+      }
+    return result;
+}
+  void output(char *string1, char *string2, int result)
+  {
+    switch (result)
+      {
+        case 0: printf("%s is equal to %s",string1,string2);
+        break;
+        case -1: printf("%s is lesser than %s",string1,string2);
+        break;
+        case 1: printf("%s is greater than %s",string1,string2);
+        break;
+      }   
+  }
+
+/*i want to find whether two given strings are equal,greater than or not equal.
     first using == operator to find both of them are equal
     hello and hello come out of loop what will be the value of i,,here i is printing again and
-     again it is not coming out of the loop,what to do to stop the iteration,apply condition*/
+     again it is not coming out of the loop,what to do to stop the iteration,apply condition
+     while (string1[i] == string2[i] )  Array out of bound
         int i=0,result = 0;
          //while (string1[i] == string2[i] && (string1[i]!='\0' && string2[i]!='\0'))
-         while (string1[i] == string2[i] && (string1[i]!='\0'))
+         while (string1[i] == string2[i] && (string1[i]!='\0')) 
 
 
- /*(string1[i] == string2[i]) this condition checks both index of string1[0] is equal to string2[0] the 
+ (string1[i] == string2[i]) this condition checks both index of string1[0] is equal to string2[0] the 
  iteretion will continue until the given values are not equal.
   If the loop completes without encountering any unequal pairs, it suggests that the entire strings are 
   equal up to the length of the shorter string.that's why we need to add one condition to check null 
@@ -42,12 +72,12 @@
   become equal.(This is completely dumb)
   (string1[i]!='\0' && string2[i]!='\0')This checks if both strings have not reached their null ('\0').
   The characters at the current position in both strings are equal.
-Neither of the strings has reached its null terminator.*/
-/*while (string1[i] == string2[i] && string1[i]!='\0' || string2[i]!='\0') even i am using || in the place of && and vice versa the loop is continueing*/
-/*given condition for termination(null value) to come out ,\0 is used to come out it is the last 
-character in string.*/
+Neither of the strings has reached its null terminator.
+while (string1[i] == string2[i] && string1[i]!='\0' || string2[i]!='\0') even i am using || in the place of && and vice versa the loop is continueing
+given condition for termination(null value) to come out ,\0 is used to come out it is the last 
+character in string.
 /*while giving || operator ./axe is not working debugging getting  result No symbol table is loaded. 
-Use the "file" command., no executables specified.*/
+Use the "file" command., no executables specified.
           {
            i++;// this block represent the exection as long as the string1[i] and string2[i] are equal
           }
@@ -99,17 +129,4 @@ Use the "file" command., no executables specified.*/
   }
 
 //try it without if statement
-//it will come out of loop as soon as it finds a difference*/
-
-  void output(char *string1, char *string2, int result)
-  {
-    switch (result)
-      {
-        case 0: printf("%s is equal to %s",string1,string2);
-        break;
-        case -1: printf("%s is lesser than %s",string1,string2);
-        break;
-        case 1: printf("%s is greater than %s",string1,string2);
-        break;
-      }   
-  }
+it will come out of loop as soon as it finds a difference*/
