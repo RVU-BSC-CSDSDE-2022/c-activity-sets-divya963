24cf42979fbef4386c64b0e48c9845d398d16f1c
divya <divyasivasankaran1998@gmail.com>
2024-02-13 18:40:26 +0530

    test

--- set02/problem05.c
+++ set02/problem05.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 
-int input(int x);
+int input();
 float borga_X(int x);
 void output(int x, float result);
 
@@ -24,12 +24,15 @@ float borga_X(int x)
 {
     float term = 1.0;
     float sum = 1.0;
+    int power ,factorial;
       
-    for(int i=1;sum<0.000001;i++)//for loop to find 1+x^1/3.. if the sum less than 0.000001 it exit 
+    for(int i=1;sum<0.000001;i++)/*for loop to find 1+x^1/3.. if the sum less than 0.000001 it exit ,increment 
+    i in each iteration*/
     {
-        find factorial
-        find square root
-        calculate factorial/squareroot sum is already fixed as 1 so 1 will add 
+        factorial *= (2*i) * (2*i+1);//find factorial
+        power *= pow(x,i);//find square root
+        term = power/factorial;//calculate factorial/squareroot sum is already fixed as 1 so 1 will add 
+        sum = sum + term;
     }
     return sum;
 }
