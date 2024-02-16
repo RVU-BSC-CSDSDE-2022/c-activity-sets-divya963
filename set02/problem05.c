12ecf1f81c565827c3bc4ecaac2d20cb04083623
divya <divyasivasankaran1998@gmail.com>
2024-02-16 15:54:11 +0530

    test

--- set02/problem05.c
+++ set02/problem05.c
@@ -23,18 +23,17 @@ float borga_X(int x)
 {
     float power = 1;
     float factorial = 1;
-    float term = 0;
+    float term = 1;
     float sum = 1;
     int i =1;
-    while((power/factorial)>=0.000001)//get rid of if inside the while loop
+    while((term)>=0.000001)//get rid of if inside the while loop
     {
         //for(int j=1;j<=((2*i)+1);j++)//No need of for loop, compute factorial from previous term.
         factorial *= (i*2) * ((2*i) + 1);
         power=power*x; 
-        term = power/factorial ;
-        //if ((term)>0.000001)
-            sum +=term; 
-            i=i+1; // we need to get 5,7... in denominator  
+        term = power/factorial ; 
+        sum += power/factorial;
+        i=i+1; // we need to get 5,7... in denominator  
     }
     return sum;  
 }
