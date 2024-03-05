60794b2aad682756bc6cf69882de5d007469a188
divya <divyasivasankaran1998@gmail.com>
2024-03-05 12:17:45 +0530

    test

--- set02/prb06.c
+++ set02/prb06.c
@@ -1,3 +1,7 @@
+/*now need to find the average.for that we need to count the odd numbers and divide it with sum of odd numbers
+already finded the sum of odd numbers now 
+1)find count of odd numbers 
+2)find average*/
 #include<stdio.h>
 int main()
 {
@@ -12,15 +16,20 @@ int main()
     }
     int arr[n];
     int sum = 0;
+    int count = 0;
+    int avg =0;
     
     printf("The odd numbers are\n");
     for(int i=0;i<n;i++){
         if(a[i]%2==1){
             arr[i] = a[i];
+            count++ ;
             printf("%d\n",arr[i]);
             sum = sum + arr[i];
+            avg = sum/count;
         }
     } 
-    printf("Sum %d",sum);
+    printf("Sum %d\n",sum);
+    printf("Average %d",avg);
     return 0;
 }
