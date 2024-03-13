07e6105ef5d43e3fd9e9ab1cbced8396bbe3bb16
divya <divyasivasankaran1998@gmail.com>
2024-03-13 14:46:57 +0530

    test

--- set02/problem07.c
+++ set02/problem07.c
@@ -20,23 +20,29 @@ void input(char *name)
 }
 int has_nice_name(char *c)
 {
-  int vowels = 0,constants =0;//use while loop without strlen
+  int vowels = 0,consonants =0;//use while loop without strlen
   //for(int i=0; i<strlen(c); i++)
   int i=0;
-  while(i<strlen(c))
+  while(c[i] != '\0')//Ascii value number (65-90A-Z,97-122a-z)
     {
       i++;
       if(c[i] == 'a' || c[i] == 'e' || c[i] == 'i' || c[i] == 'o' || c[i] == 'u' || c[i] == 'A' || c[i] == 'E' || c[i] == 'I' || c[i] == 'O' || c[i] == 'U')
       {
         vowels++;
       }
-      else
+      else if(c[i] == 'B'||c[i] == 'C'||c[i] == 'D'||c[i] == 'F'||c[i] == 'G'||c[i] == 'H'||c[i] == 'J'||
+      c[i] == 'K'||c[i] == 'L'||c[i] == 'M'||c[i] == 'N'||c[i] == 'P'||c[i] == 'Q'||c[i] == 'R'||c[i] == 'S'
+      ||c[i] == 'T'||c[i] == 'V'||c[i] == 'W'||c[i] == 'X'||c[i] == 'Y'||c[i] == 'Z'||c[i] == 'b'||c[i] == 'c'
+      ||c[i] == 'd'||c[i] == 'f'||c[i] == 'g'||c[i] == 'h'||c[i] == 'j'||
+      c[i] == 'k'||c[i] == 'l'||c[i] == 'm'||c[i] == 'n'||c[i] == 'p'||c[i] == 'q'||c[i] == 'r'||c[i] == 's'
+      ||c[i] == 't'||c[i] == 'v'||c[i] == 'w'||c[i] == 'x'||c[i] == 'y'||c[i] == 'z')/*find the consonants in the alphabets ,A-Z and a-z,but why we need to check A-Z
+      (aeiou is already checked)*/
       {
-        constants++;
+        consonants++;
       }
     }
     
-  if(vowels >= 2 && constants >= 2)
+  if(vowels >= 2 && consonants >= 2)
   {
     return 1;
   }
