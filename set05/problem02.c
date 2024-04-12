89ae39442df7c9c4120871134cc89cc6cd0f071b
divya <divyasivasankaran1998@gmail.com>
2024-04-12 13:12:44 +0530

    test

--- set05/problem02.c
+++ set05/problem02.c
@@ -5,37 +5,44 @@ typedef struct fraction{
 } Fraction;
 
 int input(int *n1,int *d1,int *n2,int *d2,int *n3,int *d3);
-find value of num/den of 3 numbers and compare the answers to find the smallest
-n1/d1 = a1 
-n2/d2 = a2 
-n3/d2 = a3 
-then compare a1,a2,a3 
-Fraction fraction();
-void compare(int a1,int a2,int a3);
-void output(int *n1,int *d1,int *n2,int *d2,int *n3,int *d3,int n,int d);//numerator and denominator need to be printed
+Fraction smallestFraction(Fraction fractions[],int size);
+void output(int n1,int d1,int n2,int d2,int n3,int d3,int num,int den);//numerator and denominator need to be printed
    
 int main()
 {
+    Fraction fraction[3];
     int n1,n2,n3,d1,d2,d3,n,d;
     input(&n1,&d1,&n2,&d2,&n3,&d3);
-    output(&n1,&d1,&n2,&d2,&n3,&d3,n,d);
+    Fraction smallest = smallestFraction(fraction,3);
+    output(n1,d1,n2,d2,n3,d3,smallest.num,smallest.den);
     return 0;
 }
 
 int input(int *n1,int *d1,int *n2,int *d2,int *n3,int *d3)
 {
-    int num,den;
     printf("Enter the numerator and denominator of 1st fraction");
     scanf("%d %d",&n1,&d1);
     printf("Enter the numerator and denomenator of 2nd fraction");
     scanf("%d",&n2,&d2);
-    printf("Enter the numerator and denomenator of 3nd fraction");
+    printf("Enter the numerator and denomenator of 3rd fraction");
     scanf("%d",&n3,&d3);
 }
 
-void output(int *n1,int *d1,int *n2,int *d2,int *n3,int *d3,int n,int d)
+Fraction smallestFraction(Fraction fractions[],int size) {
+    Fraction smallest = fractions[0];
+    for (int i = 1; i < size; i++) {
+        int product1 = smallest.num * fractions[i].den;
+        int product2 = fractions[i].num * smallest.den;
+        if (product1 > product2) {
+            smallest = fractions[i];
+        }
+    }
+    return smallest;
+}
+
+void output(int n1,int d1,int n2,int d2,int n3,int d3,int num,int den)
 {
-    printf("The smallest of %d/%d, %d/%d ,%d/%d is %d/%d",n1,d1,n2,d2,n3,d3,&n,&d);
+    printf("The smallest of %d/%d, %d/%d ,%d/%d is %d/%d",n1,d1,n2,d2,n3,d3,&num,&den);
 }
 
 
---
+++ set05/problem06.c
@@ -0,0 +1,4 @@
+
+void input_string(char *a);
+int count_words(char *string);
+void output(char *string, int no_words);
